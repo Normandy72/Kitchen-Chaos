81fc6b2c0fcd433b551dc3ce259cfc2c407d257d
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-02-20 19:55:39 +0100

    add player visualisation and rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float rotateSpeed = 10f;
 
     private void Update()
     {
@@ -32,5 +33,6 @@ public class Player : MonoBehaviour
         inputVector = inputVector.normalized;
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 }
