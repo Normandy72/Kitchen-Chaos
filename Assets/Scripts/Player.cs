de638edc67a96ae9571334e2590cb4ed32221d52
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-02-22 19:24:04 +0100

    set up new input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,36 +6,16 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private GameInput gameInput;
+
     private bool isWalking;
 
     private void Update()
-    {
-        Vector2 inputVector = new Vector2(0, 0);
-        if(Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-
-        if(Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-
-        if(Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-
-        if(Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-        
-        inputVector = inputVector.normalized;
+    {   
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
         isWalking = moveDir != Vector3.zero;
-
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);        
     }
 
