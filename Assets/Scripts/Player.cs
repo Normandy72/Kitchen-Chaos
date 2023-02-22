289f8a075b1248ae9a3806b8d88725148ea5ca47
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-02-22 18:21:16 +0100

    set animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
+    private bool isWalking;
 
     private void Update()
     {
@@ -33,6 +34,13 @@ public class Player : MonoBehaviour
         inputVector = inputVector.normalized;
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        isWalking = moveDir != Vector3.zero;
+
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);        
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
