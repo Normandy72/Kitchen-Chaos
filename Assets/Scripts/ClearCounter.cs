f94ca9c8acdac003289c90890bee5abca8848e42
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-02-23 21:44:23 +0100

    add kitchen object parent

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -6,13 +6,36 @@ public class ClearCounter : MonoBehaviour
 {
    [SerializeField] private KitchenObjectSO kitchenObjectSO;
    [SerializeField] private Transform counterTopPoint;
+   [SerializeField] private ClearCounter secondClearCounter;
+   [SerializeField] private bool testing; 
+
+   private KitchenObject kitchenObject;
+
+   private void Update()
+   {
+      if(testing && Input.GetKeyDown(KeyCode.T))
+      {
+         if(kitchenObject != null)
+         {
+            kitchenObject.SetClearCounter(secondClearCounter);
+            Debug.Log(kitchenObject.GetClearCounter());
+
+         }
+      }
+   }
 
    public void Interact()
    {
-        Debug.Log("Interact!");
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
-
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+      if(kitchenObject == null)
+      {
+         Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+         kitchenObjectTransform.localPosition = Vector3.zero;
+         kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+         kitchenObject.SetClearCounter(this);
+      }
+      else
+      {
+         Debug.Log(kitchenObject.GetClearCounter());
+      }        
    }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -6,8 +6,20 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    private ClearCounter clearCounter;
+
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        this.clearCounter = clearCounter;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
