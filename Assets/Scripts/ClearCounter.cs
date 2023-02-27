5dd2be30b51eb961c48fdcb84b7fc7126c7bf471
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-02-27 22:18:34 +0100

    player pick up and drop objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,6 +8,27 @@ public class ClearCounter : BaseCounter
 
    public override void Interact(Player player)
    {
-            
+      if(!HasKitchenObject())
+      {
+         if(player.HasKitchenObject())
+         {
+            player.GetKitchenObject().SetKitchenObjectParent(this);
+         }
+         else
+         {
+
+         }
+      }
+      else
+      {
+         if(player.HasKitchenObject())
+         {
+
+         }
+         else
+         {
+            GetKitchenObject().SetKitchenObjectParent(player);
+         }
+      }   
    }
 }
--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -11,9 +11,12 @@ public class ContainerCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+        if(!player.HasKitchenObject())
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
         
-        OnPlayerGrabbedObject? .Invoke(this, EventArgs.Empty);     
+            OnPlayerGrabbedObject? .Invoke(this, EventArgs.Empty);
+        }             
     }    
 }
