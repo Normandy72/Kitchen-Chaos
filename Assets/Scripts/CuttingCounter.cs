630689f3403d9dc8f21474f93f5bdf2b11154da0
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-02-28 20:39:47 +0100

    add world canvas

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,9 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {   
+    public event EventHandler<OnProgressChangedEventArgs> OnProgressChanged;
+    public class OnProgressChangedEventArgs : EventArgs
+    {
+        public float progressNormalized;
+    }
+
+    public event EventHandler OnCut;
+
     [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
 
     private int cuttingProgress;
@@ -18,6 +27,12 @@ public class CuttingCounter : BaseCounter
                 {
                     player.GetKitchenObject().SetKitchenObjectParent(this);
                     cuttingProgress = 0;
+
+                    CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+
+                    OnProgressChanged? .Invoke(this, new OnProgressChangedEventArgs {
+                        progressNormalized = (float)cuttingProgress / cuttingRecipeSO.cuttingProgressMax
+                    });
                 }                
             }
             else
@@ -44,7 +59,13 @@ public class CuttingCounter : BaseCounter
         {
             cuttingProgress++;
 
+            OnCut? .Invoke(this, EventArgs.Empty);
+
             CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+
+             OnProgressChanged? .Invoke(this, new OnProgressChangedEventArgs {
+                progressNormalized = (float)cuttingProgress / cuttingRecipeSO.cuttingProgressMax
+            });
 
             if(cuttingProgress >= cuttingRecipeSO.cuttingProgressMax)
             {
---
+++ Assets/Scripts/ProgressBarUI.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarUI : MonoBehaviour
+{
+    [SerializeField] private Image barImage;
+    [SerializeField] private CuttingCounter cuttingCounter;
+
+    private void Start()
+    {
+        cuttingCounter.OnProgressChanged += CuttingCounter_OnProgressChanged;
+
+        barImage.fillAmount = 0f;
+
+        Hide();
+    }
+
+    private void CuttingCounter_OnProgressChanged(object sender, CuttingCounter.OnProgressChangedEventArgs e)
+    {
+        barImage.fillAmount = e.progressNormalized;
+
+        if(e.progressNormalized == 0 || e.progressNormalized == 1)
+        {
+            Hide();
+        }
+        else
+        {
+            Show();
+        }
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
