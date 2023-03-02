2913315b8262e221cc85ed2f6ecc98dc5602601e
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-03-02 13:19:09 +0100

    add visual effects for cooking

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -1,10 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StoveCounter : BaseCounter
 {
-    private enum State
+    public event EventHandler<OnStateChangedEventArgs> OnStateChanged;
+    public class OnStateChangedEventArgs : EventArgs{
+        public State state;
+    }
+
+    public enum State
     {
         Idle,
         Frying,
@@ -42,6 +48,10 @@ public class StoveCounter : BaseCounter
                         state = State.Fried;
                         burningTimer = 0f;
                         burningRecipeSO = GetBurningRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+
+                        OnStateChanged? .Invoke(this, new OnStateChangedEventArgs{
+                            state = state
+                        });
                     }
                     break;
                 case State.Fried:
@@ -51,6 +61,10 @@ public class StoveCounter : BaseCounter
                         GetKitchenObject().DestroySelf();
                         KitchenObject.SpawnKitchenObject(burningRecipeSO.output, this);
                         state = State.Burned;
+
+                        OnStateChanged? .Invoke(this, new OnStateChangedEventArgs{
+                            state = state
+                        });
                     }
                     break;
                 case State.Burned:
@@ -73,6 +87,10 @@ public class StoveCounter : BaseCounter
 
                     state = State.Frying;
                     fryingTimer = 0f;
+
+                    OnStateChanged? .Invoke(this, new OnStateChangedEventArgs{
+                        state = state
+                    });
                 }                
             }
             else
@@ -91,6 +109,10 @@ public class StoveCounter : BaseCounter
                 GetKitchenObject().SetKitchenObjectParent(player);
 
                 state = State.Idle;
+
+                OnStateChanged? .Invoke(this, new OnStateChangedEventArgs{
+                    state = state
+                });
             }
         } 
     }
---
+++ Assets/Scripts/Counters/StoveCounterVisual.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveCounterVisual : MonoBehaviour
+{
+    [SerializeField] private StoveCounter stoveCounter;
+    [SerializeField] private GameObject stoveOnGameObject;
+    [SerializeField] private GameObject particlesGameObject;
+
+    private void Start()
+    {
+        stoveCounter.OnStateChanged += StoveCounter_OnStateChange;    
+    }
+
+    private void StoveCounter_OnStateChange(object sender, StoveCounter.OnStateChangedEventArgs e)
+    {
+        bool showVisual = e.state == StoveCounter.State.Frying || e.state == StoveCounter.State.Fried;
+        stoveOnGameObject.SetActive(showVisual);
+        particlesGameObject.SetActive(showVisual);
+    }
+}
