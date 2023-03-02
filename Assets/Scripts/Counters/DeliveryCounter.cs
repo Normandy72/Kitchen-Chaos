b1b5a75d5f3b80872a197ff75ac8d517f491ba61
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-03-02 20:51:01 +0100

    create delivery counter

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if(player.HasKitchenObject())
+        {
+            if(player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                player.GetKitchenObject().DestroySelf();
+            }            
+        }
+    }
+}
