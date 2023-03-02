61519fa8c846e09926efe667b4fdd2bff26a2543
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-03-02 12:15:52 +0100

    add stove counter

---
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveCounter : BaseCounter
+{
+    [SerializeField] private FryingRecipeSO[] fryingRecipeSOArray;
+
+    public override void Interact(Player player)
+    {
+        if(!HasKitchenObject())
+        {
+            if(player.HasKitchenObject())
+            {
+                if(HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
+                {
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                }                
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+            if(player.HasKitchenObject())
+            {
+
+            }
+            else
+            {
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        } 
+    }
+
+     private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSOWithInput(inputKitchenObjectSO);
+        
+        return fryingRecipeSO != null;
+    }
+
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSOWithInput(inputKitchenObjectSO);
+
+        if(fryingRecipeSO != null)
+        {
+            return fryingRecipeSO.output;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private FryingRecipeSO GetFryingRecipeSOWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        foreach(FryingRecipeSO fryingRecipeSO in fryingRecipeSOArray)
+        {
+            if(fryingRecipeSO.input == inputKitchenObjectSO)
+            {
+                return fryingRecipeSO;
+            }
+        }
+        return null;
+    } 
+}
--- Assets/Scripts/ScriptableObjects/FryingRecipeSO.cs
+++ Assets/Scripts/ScriptableObjects/FryingRecipeSO.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu()]
-public class CuttingRecipeSO : ScriptableObject
+public class FryingRecipeSO : ScriptableObject
 {
     public KitchenObjectSO input;
     public KitchenObjectSO output;
-    public int cuttingProgressMax;
+    public float fryingTimerMax;
 }
