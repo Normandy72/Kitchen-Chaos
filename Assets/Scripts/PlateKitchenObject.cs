aa1a8834688c14acd0dc5035ba03c69460d4ed47
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-03-02 20:25:24 +0100

    add icons above the plate

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -38,4 +38,9 @@ public class PlateKitchenObject : KitchenObject
             return true;
         }            
     }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
+    }
 }
