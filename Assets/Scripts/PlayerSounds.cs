6501228cd404c247e8cf01125ba771a0e63701bc
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-03-03 19:16:46 +0100

    add foot steps sound

---
+++ Assets/Scripts/PlayerSounds.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+    private Player player;
+    private float footstepTimer;
+    private float footstepTimerMax = 0.1f;
+
+    private void Awake()
+    {
+        player = GetComponent<Player>();    
+    }
+
+    private void Update()
+    {
+        footstepTimer -= Time.deltaTime;
+
+        if(footstepTimer < 0)
+        {
+            footstepTimer = footstepTimerMax;
+
+            if(player.IsWalking())
+            {
+                float volume = 1f;
+                SoundManager.Instance.PlayFootstepsSound(player.transform.position, volume);
+            }            
+        } 
+    }
+}
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager Instance {get; private set;}
+
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start() {
         DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
@@ -58,5 +65,10 @@ public class SoundManager : MonoBehaviour
     private void PlaySound(AudioClip[] audioClipArray, Vector3 position, float volume = 1f)
     {
         PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position, volume);
+    }
+
+    public void PlayFootstepsSound(Vector3 position, float volume)
+    {
+        PlaySound(audioClipRefsSO.footstep, position);
     }
 }
