7772828e81d02ad37b8bd329457ad4fe05d507d5
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-03-03 21:25:02 +0100

    create main menu, play button and quit button

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() => {
+
+        });
+
+        quitButton.onClick.AddListener(() => {
+            Application.Quit();
+        }); 
+    }
+}
