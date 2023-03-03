cb21d9efa44e3d0f2aea13686f776e8a50d4bf7d
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-03-03 16:57:19 +0100

    spawn recipe template

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour
 {
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
+
     public static DeliveryManager Instance{get; private set;} 
 
     [SerializeField] private RecipeListSO recipeListSO;
@@ -28,9 +32,10 @@ public class DeliveryManager : MonoBehaviour
 
             if(waitingRecipeSOList.Count < waitingRecipesMax)
             {
-                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
-                Debug.Log(waitingRecipeSO.recipeName);
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
                 waitingRecipeSOList.Add(waitingRecipeSO);
+
+                OnRecipeSpawned? .Invoke(this, EventArgs.Empty);
             }            
         }
     }
@@ -62,12 +67,18 @@ public class DeliveryManager : MonoBehaviour
                 }
                 if(plateContentsMatchesRecipe)
                 {
-                    Debug.Log("Player delivered the correct recipe!");
                     waitingRecipeSOList.RemoveAt(i);
+
+                    OnRecipeCompleted? .Invoke(this, EventArgs.Empty);
+
                     return;
                 }
             }
         }
-        Debug.Log("Player did not deliver a correct recipe!");
+    }
+
+    public List<RecipeSO> GetWaitingRecipeSOList()
+    {
+        return waitingRecipeSOList;
     }
 }
--- Assets/Scripts/DeliveryManagerUI.cs
+++ Assets/Scripts/DeliveryManagerUI.cs
@@ -4,5 +4,47 @@ using UnityEngine;
 
 public class DeliveryManagerUI : MonoBehaviour
 {
-    
+    [SerializeField] private Transform container;
+    [SerializeField] private Transform recipeTemplate;
+
+    private void Awake()
+    {
+        recipeTemplate.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        DeliveryManager.Instance.OnRecipeSpawned += DeliveryManager_OnRecipeSpawned;
+        DeliveryManager.Instance.OnRecipeCompleted += DeliveryManager_OnRecipeCompleted;
+
+        UpdateVisual();
+    }
+
+    private void DeliveryManager_OnRecipeSpawned(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void DeliveryManager_OnRecipeCompleted(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        foreach(Transform child in container)
+        {
+            if(child == recipeTemplate)
+            {
+                continue;
+            }
+            Destroy(child.gameObject);
+        }
+
+        foreach(RecipeSO recipeSO in DeliveryManager.Instance.GetWaitingRecipeSOList())
+        {
+            Transform recipeTransform = Instantiate(recipeTemplate, container);
+            recipeTransform.gameObject.SetActive(true);
+        }
+    }
 }
