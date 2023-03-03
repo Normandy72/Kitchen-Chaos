19060bd1ff9086b91b854dfd234fbae10b808f9b
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-03-03 19:50:40 +0100

    create state machine for game manager

---
+++ Assets/Scripts/KitchenGameManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenGameManager : MonoBehaviour
+{
+    private enum State
+    {
+        WaitingToStart,
+        CutdownToStart,
+        GamePlaying,
+        GameOver,
+    }
+
+    private State state;
+    private float waitingToStartTimer = 1f;
+    private float countdownToStartTimer = 3f;
+    private float gamePlayingTimer = 10f;
+
+    private void Awake()
+    {
+        state = State.WaitingToStart;
+    }
+
+    private void Update()
+    {
+        switch(state)
+        {
+            case State.WaitingToStart:
+                waitingToStartTimer -= Time.deltaTime;
+                if(waitingToStartTimer < 0f)
+                {
+                    state = State.CutdownToStart;
+                }
+                break;
+            case State.CutdownToStart:
+                countdownToStartTimer -= Time.deltaTime;
+                if(countdownToStartTimer < 0f)
+                {
+                    state = State.GamePlaying;
+                }
+                break;
+            case State.GamePlaying:
+                gamePlayingTimer -= Time.deltaTime;
+                if(gamePlayingTimer < 0f)
+                {
+                    state = State.GameOver;
+                }
+                break;
+            case State.GameOver:
+                break;
+        }
+    } 
+
+
+}
